2df7631978540fdb5557c9d69672e01fce11da93
Patrick Chung <kchungsemchong@gmail.com>
2015-10-17 22:11:59 +0400

    AddSupplier

---
+++ BontoBuyWebApplication/Models/UserRole/Supplier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BontoBuyWebApplication.Models.UserRole
+{
+    [Table("Supplier")]
+    public class Supplier : User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SupplierId { get; set; }
+        public string SupplierName { get; set; }
+        public string Street { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Website { get; set; }
+        public string Status { get; set; }
+        public string City { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+
+
+    }
+}
